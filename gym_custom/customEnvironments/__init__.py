a85c965bea22056c39a31d9160b6f0ea522d9ff0
fliegla <43549412+fliegla@users.noreply.github.com>
2022-10-10 14:57:00 +0200

    Update __init__.py

--- gym_custom/customEnvironments/__init__.py
+++ gym_custom/customEnvironments/__init__.py
@@ -1,7 +1,6 @@
 from gym.envs.registration import register
 
 register(
-  id='DiffDrive-v0',
-  entry_point='customEnvironments.envs:DiffDriveEnv',
-  max_episode_steps=1000,
+  id="DiffDrive-v0",
+  entry_point="customEnvironments.envs:DiffDriveEnv",
 )
